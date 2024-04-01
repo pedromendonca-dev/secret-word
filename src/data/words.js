2f9d6325572c054fbf320bd78d8474484aa72afb
Pedro Mendonça <121640828+pedromendonca-dev@users.noreply.github.com>
2024-03-31 22:47:13 -0300

    publish

---
+++ src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList ={
+    carro: ["motor","pneu","parabrisa","parachoque","vidro", "freio", "macha"],
+    fruta: ["maracuja","maça","abacaxi","banana","laranja"],
+    corpo: ["cotovelo","abdomem","estomago","unha","dente"],
+    programação: ["framework","linguagem","InteligenciaArtificial"],
+    alimento: ["arroz","leite","feijao","strogonoff"]
+  };
